cd557681be8c331cdddc2812c8e168a98d746b26
alimy <alimy@gility.net>
2021-01-07 22:27:14 +0800

    use github.com/alimy/embedx process change root for embed.FS

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alimy/mirage-ui
 
 go 1.16
+
+require github.com/alimy/embedx v0.3.2
--- portal.go
+++ portal.go
@@ -1,47 +1,24 @@
+// Copyright 2021 Michael Li <alimy@gility.net>. All rights reserved.
+// Use of this source code is governed by GNU General Public License 2.0 that
+// can be found in the LICENSE file.
+
 package ui
 
 import (
 	"embed"
-	"io/fs"
 	"net/http"
-	"strings"
+
+	"github.com/alimy/embedx"
 )
-
-//go:embed portal/dist/css
-//go:embed portal/dist/js/*.js
-//go:embed portal/dist/favicon.ico
-//go:embed portal/dist/index.html
-var content embed.FS
-
-type embedFS struct {
-	prefix string
-}
-
-// Open opens the named file for reading and returns it as an fs.File.
-func (f *embedFS) Open(name string) (fs.File, error) {
-	return content.Open(f.path(name))
-}
-
-// ReadDir reads and returns the entire named directory.
-func (f *embedFS) ReadDir(name string) ([]fs.DirEntry, error) {
-	return content.ReadDir(f.path(name))
-}
-
-// ReadFile reads and returns the content of the named file.
-func (f *embedFS) ReadFile(name string) ([]byte, error) {
-	return content.ReadFile(f.path(name))
-}
-
-func (f *embedFS) path(name string) string {
-	if name == "." {
-		return f.prefix
-	}
-	return strings.Join([]string{f.prefix, name}, "/")
-}
 
 // NewFileSystem get a http.FileSystem instance
 func NewFileSystem() http.FileSystem {
-	return http.FS(&embedFS{
-		prefix: "portal/dist",
-	})
+	//go:embed portal/dist/css
+	//go:embed portal/dist/js/*.js
+	//go:embed portal/dist/favicon.ico
+	//go:embed portal/dist/index.html
+	var content embed.FS
+
+	embedFS := embedx.NewFileSystem(&content, embedx.ChangeRoot("portal/dist"))
+	return http.FS(embedFS)
 }
